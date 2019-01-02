b2f25c767f33380634c1b29aadef73b45288470f
thematthewmullins <thematthewmullins@gmail.com>
2019-01-02 11:21:28 -0500

    Module 3 fixes

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,7 +4,7 @@ class QuizQuestionButton extends Component {
     render() {
         return (
             <li>
-                <button>{button_text}</button>
+                <button>{this.props.button_text}</button>
             </li>
         )
     }
