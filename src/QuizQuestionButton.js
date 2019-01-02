cfbbe860ca1af1c1041a855ab347c86c3b6981d2
thematthewmullins <thematthewmullins@gmail.com>
2019-01-02 11:15:23 -0500

    Module 3

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class QuizQuestionButton extends Component {
+    render() {
+        return (
+            <li>
+                <button>{button_text}</button>
+            </li>
+        )
+    }
+}
+
+export default QuizQuestionButton
